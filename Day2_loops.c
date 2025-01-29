4a1031e1295e2fb37a2819de8b59d358f8746728
Virendra Singh <virendrasingh.v12@gmail.com>
2025-01-30 00:12:58 +0530

    coding

--- Day2_loops.c
+++ Day2_loops.c
@@ -1,7 +1,28 @@
 #include<stdio.h>
+
+void check (int n)
+{
+    if (n > 0)
+    {
+        printf("%d is an positive number", n);
+    }
+    else if (n==0)
+    {
+        printf("%d is zero", n);
+    }
+    else {
+        printf("%d is an negative number", n);
+    }
+}
+
+
+
 int main()
 {
-    
+    int k;
+    printf("enetr the number = ");
+    scanf("%d", &k);
 
+    check(k);
     return 0;
 }
